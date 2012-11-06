8d8eeb5e4c70a2275cf67a8bc3d84c40bb246976
Gerhard Schöfl <gschofl@yahoo.de>
2012-11-06 12:00:17 +0100

    coerce DNAString to DNAbin and vice versa

--- R/package.r
+++ R/package.r
@@ -1,9 +1,20 @@
-#' The blastr package provides an interface to NCBI's
-#' Blast utilities.
+#' Provide a set of classes and functions to manage
+#' NCBI Blast web service queries via R.
 #'
-#' @name blastr-package
-#' @author Gerhard Schöfl \email{gschofl@@yahoo.de}
+#'  \tabular{ll}{
+#'    Package: \tab blastr\cr
+#'    Type: \tab Package\cr
+#'    Version: \tab 0.0-2\cr
+#'    Date: \tab 2012-11-05\cr
+#'    License: \tab MIT\cr
+#'  }
+#'
+#' @author
+#' Gerhard Schoefl \email{gschof@@yahoo.de}
+#'
+#' Maintainer: Gerhard Schoefl \email{gschof@@yahoo.de}
+#' @name blastr
 #' @docType package
+#' @title A wrapper arpound NCBI Blast.
 #' @keywords package
-NA
-####
+NULL
---
+++ R/utils.r
@@ -0,0 +1,43 @@
+#' @importFrom ape read.dna
+#' @importFrom ape write.dna
+#' @importFrom Biostrings writeXStringSet
+#' @importFrom Biostrings readDNAStringSet
+#' @importFrom Biostrings readDNAMultipleAlignment
+#' @import methods
+setOldClass("DNAbin")
+
+
+setAs("XStringSet", "DNAbin", function (from) {
+  tmp <- tempfile()
+  on.exit(unlink(tmp))
+  writeXStringSet(from, filepath=tmp, format="fasta")
+  to <- read.dna(tmp, format="fasta")
+  to
+})
+
+
+setAs("DNAbin", "DNAStringSet", function (from) {
+  tmp <- tempfile()
+  on.exit(unlink(tmp))
+  write.dna(from, file=tmp, format="fasta")
+  to <- readDNAStringSet(tmp, "fasta")
+  to
+})
+
+setAs("DNAbin", "DNAMultipleAlignment", function (from) {
+  tmp <- tempfile()
+  on.exit(unlink(tmp))
+  write.dna(from, file=tmp, format="fasta")
+  to <- readDNAMultipleAlignment(tmp, "fasta")
+  to
+})
+
+
+setAs("XStringSet", "DNAMultipleAlignment", function (from) {
+  tmp <- tempfile()
+  on.exit(unlink(tmp))
+  writeXStringSet(from, filepath=tmp, format="fasta")
+  to <- readDNAMultipleAlignment(tmp, "fasta")
+  to
+})
+
